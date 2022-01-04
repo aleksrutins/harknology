5d47425736907f362aaa5b87072793b44ba41c74
Aleks Rutins <aleks@rutins.com>
2022-01-04 12:23:41 -0500

    Increase student name size

--- src/components/UserDisplay.tsx
+++ src/components/UserDisplay.tsx
@@ -7,9 +7,9 @@ const UserDisplay: FunctionComponent<{email: string, badge?: boolean}> = (props)
     const {data, error} = useSWR<UserResponse>('/api/user/' + props.email, json);
     const [nameVisible, setNameVisible] = useState(!!!props.badge);
     return error ? <span>Error fetching user {props.email}</span> : 
-    (data? <span className={`leading-3 text-[0.75rem] ${props.badge ? 'hover:p-1 rounded-full hover:border border-gray-200' : ''}`} onMouseEnter={() => setNameVisible(true)} onMouseLeave={props.badge ? () => setNameVisible(false) : () => {}}>
-        <img className="inline rounded-full h-[0.75rem] mr-[2px]" src={data?.image} alt={data?.email} />
-        {nameVisible? <span>{data?.name}</span> : null}
+    (data? <span className={`leading-3 ${props.badge ? 'hover:p-1 rounded-full hover:border border-gray-200 text-base' : 'text-xs'}`} onMouseEnter={() => setNameVisible(true)} onMouseLeave={props.badge ? () => setNameVisible(false) : () => {}}>
+        <img className={`inline rounded-full mr-[2px] ${props.badge? 'h-4' : 'h-3'} align-center`} src={data?.image} alt={data?.email} />
+        {nameVisible? <span className="align-center">{data?.name}</span> : null}
     </span> : <span>{props.email}</span>)
 };
 
