aebbdfaa7c7c94ebb556d1bd021544763606e21c
Aleks Rutins <aleks@rutins.com>
2022-01-08 09:27:01 -0500

    Return something so the build doesn't fail

--- pages/discussions/view.tsx
+++ pages/discussions/view.tsx
@@ -2,7 +2,7 @@ import { useAuth } from "@/auth";
 import LoadingIndicator from "@/components/LoadingIndicator";
 
 export default function DiscussionView() {
-    const {session, status} = useAuth();
-    
+    const { session, status } = useAuth();
+    return <></>
 }
 
