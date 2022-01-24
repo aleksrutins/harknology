5d5137212a43e2d71072a242a4e764bf5076b82f
Aleks Rutins <aleks@rutins.com>
2022-01-24 18:14:48 -0500

    Fix type error

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,12 +1,12 @@
 import Link from "next/link";
-import React, { FunctionComponent, ReactNode } from "react"
+import React, { FunctionComponent, HTMLAttributes, ReactNode } from "react"
 
 const Card: FunctionComponent<{ 
     title: string,
     href?: string, 
     onClick?: (...args: any[]) => any, 
     cardType?: 'item' | 'placeholder',
-    icon?: React.ComponentType
+    icon?: React.ComponentType<{className?: string}>
 }> = (props) => {
     let a = <a onClick={props.onClick ?? (() => { })} className={`cursor-pointer block hover:shadow-green-500/50 rounded-md ${props.children != undefined || 'flex flex-col justify-center items-center'} w-[300px] h-[190px] p-6 m-6 hover:text-green-700 transition hover:shadow-lg border border-grey-400 hover:border-green-400 ${props.cardType == 'placeholder' && 'border-dashed text-gray-400 hover:border-solid'}`}>
         <h2 className="font-light text-lg">
