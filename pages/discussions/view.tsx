7497926d79297c5b701cb1716cfd01e26f6755bd
Aleks Rutins <aleks@rutins.com>
2022-02-21 12:51:22 -0500

    Text editor!

--- pages/discussions/view.tsx
+++ pages/discussions/view.tsx
@@ -1,6 +1,7 @@
 import { useAuth } from "@/auth";
 import Loader from "@/components/Loader";
 import LoadingIndicator from "@/components/LoadingIndicator";
+import ResponseEditor from "@/components/ResponseEditor";
 import { Toast } from "@/components/Toast";
 import json from "@/json";
 import { Discussion } from "@prisma/client";
@@ -14,9 +15,12 @@ export default function DiscussionView() {
     return <>
         {error && <Toast background="#ef4444">Error loading discussion</Toast>}
         <Loader depends={data} borderColor="black" center>
-            <h1 className="text-2xl text-center font-light mb-0 pb-0">{data?.name}</h1>
-            <p className="max-w-2xl mx-auto whitespace-pre-wrap">{data?.description}</p>
+            <div className="max-w-3xl mx-auto">
+                <h1 className="text-2xl text-center font-light mb-0 pb-0">{data?.name}</h1>
+                <p className="max-w-2xl mx-auto whitespace-pre-wrap">{data?.description}</p>
 
+                <ResponseEditor discussion={data?.id!} />
+            </div>
         </Loader>
     </>;
 }
