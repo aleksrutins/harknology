d51f45077be764843992c0e86e643d6dd8d41dbf
Aleks Rūtiņš <aleks@rutins.com>
2022-03-21 14:19:43 -0400

    Fix chevron size

--- src/components/Basic/Accordion.tsx
+++ src/components/Basic/Accordion.tsx
@@ -9,7 +9,7 @@ const Accordion: FC<{
     return <div className="rounded border">
         <h2 className={`p-2 ${isOpen && 'border-b'} cursor-pointer`} onClick={() => setOpen(!isOpen)}>
             <span>{props.title}</span>
-            <span className="float-right">{isOpen? <ChevronDoubleDownIcon/> : <ChevronDoubleLeftIcon/>}</span>
+            <span className="float-right">{isOpen? <ChevronDoubleDownIcon className="w-5 h-5"/> : <ChevronDoubleLeftIcon className="w-5 h-5"/>}</span>
         </h2>
         <div className={`p-3 ${isOpen? 'block' : 'hidden'}`}>
             {props.children}
