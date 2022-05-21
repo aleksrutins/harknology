72464f0e30b63073f32ba7dc60953513e81e3b07
Aleks Rutins <aleks@rutins.com>
2022-05-21 18:27:44 -0400

    Display response dates

--- src/components/ResponseDisplay.tsx
+++ src/components/ResponseDisplay.tsx
@@ -20,21 +20,24 @@ const ResponseDisplay: FC<Props> = (props) => {
     const [editing, setEditing] = useState(false);
 
     return <div className={`rounded hover:border-gray-400 bg-white transition my-3 border ${editing && '!border-gray-600'}`}>
-        <div className={`sticky w-100 bg-white backdrop-blur-sm p-3 border-b ${editing && 'border-gray-600'} rounded-t`} style={{top: props.depth * 40, zIndex: 99 - props.depth}}>
-            <UserDisplay email={props.response.userEmail}/>
-            <ToggleButton active={editing} className="group float-right mt-[-9px] mr-[-9px]" buttonStyle="primary" onClick={(e: MouseEvent) => {e.stopPropagation(); setEditing(!editing)}}>
-                    <PlusIcon className="inline w-5 h-5 align-middle"/>
+        <div className={`sticky w-100 bg-white backdrop-blur-sm p-3 border-b ${editing && 'border-gray-600'} rounded-t`} style={{ top: props.depth * 40, zIndex: 99 - props.depth }}>
+            <UserDisplay email={props.response.userEmail} />
+            <div className="float-right">
+                <small>{props.response.lastModified.toLocaleString()}</small>
+                <ToggleButton active={editing} className="group float-right mt-[-9px] mr-[-9px]" buttonStyle="primary" onClick={(e: MouseEvent) => { e.stopPropagation(); setEditing(!editing) }}>
+                    <PlusIcon className="inline w-5 h-5 align-middle" />
                     <span className="hidden group-hover:inline align-middle pl-1">Respond</span>
                 </ToggleButton>
+            </div>
         </div>
         <div className="p-2">
-            <p dangerouslySetInnerHTML={{__html: props.response.content}}></p>
+            <p dangerouslySetInnerHTML={{ __html: props.response.content }}></p>
             {(responses?.length ?? 0 > 0) ?
-            <Accordion initiallyOpen={false} title={`Responses${(responses?.length ?? 0) > 0? ` (${responses?.length})` : ``}`}>
-                {responses?.map(response => <ResponseDisplay key={response.id} {...{response}} swr={props.swr} depth={props.depth + 1}></ResponseDisplay>)}
-            </Accordion>
-            : ''}
-            {editing && <ResponseEditor discussion={props.response.discussionId!} parent={props.response.id} swr={props.swr}/>}
+                <Accordion initiallyOpen={false} title={`Responses${(responses?.length ?? 0) > 0 ? ` (${responses?.length})` : ``}`}>
+                    {responses?.map(response => <ResponseDisplay key={response.id} {...{ response }} swr={props.swr} depth={props.depth + 1}></ResponseDisplay>)}
+                </Accordion>
+                : ''}
+            {editing && <ResponseEditor discussion={props.response.discussionId!} parent={props.response.id} swr={props.swr} />}
         </div>
     </div>;
 }
