f1301d27d7ade08278de650d6de9c774825384f5
Aleks Rutins <aleks@rutins.com>
2022-09-12 15:50:55 -0400

    Pad loader in class page

--- src/components/LoadingIndicator.tsx
+++ src/components/LoadingIndicator.tsx
@@ -2,13 +2,14 @@ import { FunctionComponent } from "react";
 import styles from '@~/styles/LoadingIndicator.module.css';
 
 export type Props = {
-    borderColor: 'white' | 'black',
-    center?: boolean
+  borderColor: 'white' | 'black',
+  center?: boolean,
+  pad?: boolean
 }
 
-const LoadingIndicator: FunctionComponent<Props> = (props) => 
-    <span className={`${styles.loadingIndicator} ${props.center && styles.center}`} style={{borderColor: props.borderColor}}>
-        <span className={styles.loadingIndicatorCenter} style={{borderColor: props.borderColor}}></span>
-    </span>
+const LoadingIndicator: FunctionComponent<Props> = (props) =>
+  <span className={`${styles.loadingIndicator} ${props.center && styles.center} ${props.pad && 'relative top-3'}`} style={{ borderColor: props.borderColor }}>
+    <span className={styles.loadingIndicatorCenter} style={{ borderColor: props.borderColor }}></span>
+  </span>
 
 export default LoadingIndicator;
--- src/functions/checkClassAuth.ts
+++ src/functions/checkClassAuth.ts
@@ -4,27 +4,27 @@ import { Session } from "next-auth";
 
 export type Result = [[(Class & { students: User[], discussions: Discussion[] }) | null, 'teacher' | 'student' | 'none'], boolean];
 
-export default async (id: string, session: Session): Promise<Result> => {
-    const cls = prisma.class.findUnique({
-        where: {
-            id: id
-        },
-        select: {
-            students: true,
-            discussions: true,
-            teacherEmail: true,
-            id: true,
-            name: true,
-            description: true
-        }
-    });
-    try {
-        if((await cls)?.teacherEmail == session.user?.email) {
-        return [[await cls, 'teacher'], true];
-    } else if(
-          (await cls.students()).map(student => student.email).includes(session.user?.email!)) {
-              return [[await cls, 'student'], true];
-          }
-        } catch {}
-    return [[null, 'none'], false];
+export default async function checkClassAuth(id: string, session: Session): Promise<Result> {
+  const cls = prisma.class.findUnique({
+    where: {
+      id: id
+    },
+    select: {
+      students: true,
+      discussions: true,
+      teacherEmail: true,
+      id: true,
+      name: true,
+      description: true
+    }
+  });
+  try {
+    if ((await cls)?.teacherEmail == session.user?.email) {
+      return [[await cls, 'teacher'], true];
+    } else if (
+      (await cls.students()).map(student => student.email).includes(session.user?.email!)) {
+      return [[await cls, 'student'], true];
+    }
+  } catch { }
+  return [[null, 'none'], false];
 }
