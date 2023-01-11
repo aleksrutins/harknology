69321a2cfc9f5f9173ac32b56c356d441cfc4754
Aleks Rutins <aleks@rutins.com>
2023-01-10 20:25:24 -0500

    Center banner image

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -2,5 +2,5 @@ import Image from "next/image";
 import banner from "@res:banner.svg";
 
 export default function Banner() {
-    return <Image src={banner} width={500} height={128} alt="Harknology" className="text-center block"/>
+    return <Image src={banner} width={500} height={128} alt="Harknology" className="text-center block" style={{margin: "auto"}}/>
 }
