3d13b97f46b2514126d6d1b1b24abf2f1cae05cb
Aleks Rutins <aleks@rutins.com>
2023-05-07 19:47:30 -0400

    It's Deno time

--- app/components/Banner.tsx
+++
@@ -1,6 +0,0 @@
-import Image from "next/image";
-import banner from "@res:banner.png";
-
-export default function Banner() {
-    return <Image src={banner} width={500} alt="Harknology" className="text-center block" style={{margin: "auto"}}/>
-}
--- pages-old/discussions.tsx
+++
@@ -1,7 +0,0 @@
-import { DashboardContent } from "@/components/DashboardLayout";
-
-export default function DiscussionsList() {
-    return <DashboardContent>
-        <h1>Discussions</h1>
-    </DashboardContent>
-}
