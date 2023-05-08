4c5dbb70d7272b4d0fa9124aa54995bc400f8c20
Aleks Rutins <aleks@rutins.com>
2023-05-08 17:30:58 -0400

    Indicate active route

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,11 @@
 <script setup lang="ts">
-import { HomeIcon, AcademicCapIcon } from '@heroicons/vue/20/solid'
+import { HomeIcon, AcademicCapIcon } from '@heroicons/vue/24/outline'
+import { HomeIcon as HomeSolid, AcademicCapIcon as AcademicSolid } from '@heroicons/vue/24/solid'
 import LoadingIndicator from './components/LoadingIndicator.vue';
 import EnsureAuth from './components/EnsureAuth.vue';
 const routes = [
-  { path: '/', icon: HomeIcon, title: 'Home' },
-  { path: '/classes', icon: AcademicCapIcon, title: 'Classes' }
+  { path: '/', name: 'home', icon: [HomeIcon, HomeSolid], title: 'Home' },
+  { path: '/classes', name: 'classes', icon: [AcademicCapIcon, AcademicSolid], title: 'Classes' }
 ];
 </script>
 <template>
@@ -15,7 +16,7 @@ const routes = [
           <div class="flex flex-col">
             <router-link v-for="route in routes" v-bind:to="route.path"
               class="rounded-lg flex flex-row items-center py-3 px-4 m-2 hover:bg-gray-300 transition">
-              <component v-bind:is="route.icon" style="width: 20px;" />
+              <component v-bind:is="$route.matched.some(({ name }) => name == route.name) ? route.icon[1] : route.icon[0]" style="width: 20px;" />
               <p class="pl-2">
                 {{ route.title }}
               </p>
--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,13 @@
 import { createApp } from 'vue'
-import { createRouter, createWebHistory } from 'vue-router'
+import { RouteRecordRaw, createRouter, createWebHistory } from 'vue-router'
 import './style.css'
 import App from './App.vue'
 import Home from './pages/Home.vue'
 import Classes from './pages/Classes.vue'
 
-const routes = [
-    { path: '/', component: Home },
-    { path: '/classes', component: Classes }
+const routes: RouteRecordRaw[] = [
+    { path: '/', name: 'home', component: Home },
+    { path: '/classes', name: 'classes', component: Classes }
 ]
 
 const router = createRouter({
