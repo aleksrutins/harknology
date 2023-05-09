6b26aa498ee22bea1ddc4dd1c3704032bc65d8b4
Aleks Rutins <aleks@rutins.com>
2023-05-08 20:20:56 -0400

    Lazy load routes

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,10 @@ import { createApp } from 'vue'
 import { RouteRecordRaw, createRouter, createWebHistory } from 'vue-router'
 import './style.css'
 import App from './App.vue'
-import Home from './pages/Home.vue'
-import Classes from './pages/Classes.vue'
 
 const routes: RouteRecordRaw[] = [
-    { path: '/', name: 'home', component: Home },
-    { path: '/classes', name: 'classes', component: Classes }
+    { path: '/', name: 'home', component: () => import('./pages/Home.vue') },
+    { path: '/classes', name: 'classes', component: () => import('./pages/Classes.vue') }
 ]
 
 const router = createRouter({
