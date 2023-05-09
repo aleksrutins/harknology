a3f71a3afa3f74be7bde2ecc5f0c6703e7f6f405
Aleks Rutins <aleks@rutins.com>
2023-05-08 21:10:32 -0400

    Add a function

--- src/pages/Classes.vue
+++ src/pages/Classes.vue
@@ -1,3 +1,15 @@
+<script setup lang="ts">
+    import { supabase } from '../util/supabase';
+
+    const classes = await supabase.from('classes').select('*');
+    classes;
+</script>
 <template>
-    Classes
+    <div class="flex flex-col items-center p-2">
+        <div class="max-w-6xl flex-grow">
+            <h1 class="text-2xl">Classes</h1>
+
+
+        </div>
+    </div>
 </template>
---
+++ supabase/functions/get-user/index.ts
@@ -0,0 +1,39 @@
+// Follow this setup guide to integrate the Deno language server with your editor:
+// https://deno.land/manual/getting_started/setup_your_environment
+// This enables autocomplete, go to definition, etc.
+
+import { serve } from "https://deno.land/std@0.168.0/http/server.ts"
+import * as postgres from 'https://deno.land/x/postgres@v0.14.2/mod.ts'
+
+const databaseUrl = Deno.env.get('SUPABASE_DB_URL')!
+
+const pool = new postgres.Pool(databaseUrl, 3, true)
+
+serve(async (req) => {
+  try {
+    const { uid } = await req.json()
+    const connection = await pool.connect();
+    try {
+      const result = await connection.queryObject`SELECT email FROM auth.users WHERE id = ${uid}`;
+      return new Response(JSON.stringify({
+        email: (result.rows[0] as {email: string})?.email
+      }), {
+        status: 200,
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      })
+    } finally {
+      connection.release()
+    }
+  } catch(err) {
+    console.error(err)
+    return new Response(String(err?.message ?? err), { status: 500 })
+  }
+})
+
+// To invoke:
+// curl -i --location --request POST 'http://localhost:54321/functions/v1/' \
+//   --header 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0' \
+//   --header 'Content-Type: application/json' \
+//   --data '{"name":"Functions"}'
