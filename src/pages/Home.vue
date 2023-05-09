caf9d3dfe51abf478caa20733d5a7dca48949bfa
Aleks Rutins <aleks@rutins.com>
2023-05-08 20:45:30 -0400

    Yet another new logo

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,1 +1,8 @@
-<template></template>
+<template>
+    <div class="flex flex-col items-center p-5">
+        <div>
+            <img src="/banner.svg" class="h-[128px]" alt="Harknology">
+        </div>
+        <p>Manage your class's discussions with ease.</p>
+    </div>
+</template>
