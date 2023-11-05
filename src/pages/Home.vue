511d1a2faf7fe872a031eb1570a39a060f524551
Aleks Rūtiņš <aleks@rutins.com>
2023-11-05 08:53:47 -0500

    NextJS

--- src/pages/Home.vue
+++
@@ -1,8 +0,0 @@
-<template>
-    <div class="flex flex-col items-center p-5">
-        <div>
-            <img src="/banner.svg" class="h-[128px]" alt="Harknology">
-        </div>
-        <p>Manage your class's discussions with ease.</p>
-    </div>
-</template>
--- src/util/ellipsize.ts
+++
@@ -1,3 +0,0 @@
-export function ellipsize(str: string, maxLen: number): string {
-    return str.slice(0, maxLen) + (str.length > maxLen? '\u2026' : '');
-}
