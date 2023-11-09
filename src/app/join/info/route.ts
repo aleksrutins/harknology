de38ddc1a6a2b1e3df5f36d055949b961e44dead
Aleks Rutins <aleks@rutins.com>
2023-11-09 11:53:04 +0000

    Use a valid error response code

--- src/app/join/info/route.ts
+++ src/app/join/info/route.ts
@@ -1,13 +1,17 @@
 import { getClassInfo } from "../join";
 
-export type JoinClassInfo = ReturnType<typeof getClassInfo> extends Promise<infer T> ? T : any
+export type JoinClassInfo = ReturnType<typeof getClassInfo> extends Promise<
+  infer T
+>
+  ? T
+  : any;
 
 export async function GET(req: Request) {
-    const params = new URL(req.url).searchParams;
-    const code = params.get('code');
-    if(!code) return Response.error();
-    const info = await getClassInfo(code);
-    if(!info) return Response.error();
+  const params = new URL(req.url).searchParams;
+  const code = params.get("code");
+  if (!code) return new Response("Invalid code", { status: 400 });
+  const info = await getClassInfo(code);
+  if (!info) return new Response("Invalid code", { status: 400 });
 
-    return Response.json(info);
+  return new Response(JSON.stringify(info));
 }
