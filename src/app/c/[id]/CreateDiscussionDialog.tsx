9861a2d29c9119c0dc492f44e062252323077892
Aleks Rūtiņš <aleks@rutins.com>
2023-11-09 10:56:51 -0500

    rich-text discussion descriptions

--- src/app/c/[id]/CreateDiscussionDialog.tsx
+++ src/app/c/[id]/CreateDiscussionDialog.tsx
@@ -1,49 +1,86 @@
+import Tiptap, {
+    TiptapContent,
+    TiptapControls,
+    TiptapFormCompat,
+} from "@/app/components/Tiptap";
 import prisma from "@/lib/prisma";
 import { PlusIcon } from "@radix-ui/react-icons";
-import { Button, DialogClose, DialogContent, DialogDescription, DialogRoot, DialogTitle, DialogTrigger, Flex, IconButton, TextArea, TextFieldInput } from "@radix-ui/themes";
+import {
+    Button,
+    Card,
+    DialogClose,
+    DialogContent,
+    DialogDescription,
+    DialogRoot,
+    DialogTitle,
+    DialogTrigger,
+    Flex,
+    IconButton,
+    TextArea,
+    TextFieldInput,
+} from "@radix-ui/themes";
 import { revalidateTag } from "next/cache";
 import { redirect } from "next/navigation";
 
-export default function CreateDiscussionDialog({ classId }: { classId: string }) {
+export default function CreateDiscussionDialog({
+    classId,
+}: {
+    classId: string;
+}) {
     async function createDiscussion(form: FormData) {
-        'use server';
+        "use server";
         const { id } = await prisma.discussion.create({
             data: {
-                name: form.get('name') as string,
-                description: form.get('description') as string,
-                class_id: classId
+                name: form.get("name") as string,
+                description: form.get("content") as string,
+                class_id: classId,
             },
         });
 
-        revalidateTag('discussions');
+        revalidateTag("discussions");
         redirect(`/c/${classId}/d/${id}`);
     }
-    
-    return <DialogRoot>
-        <DialogTrigger>
-            <IconButton radius="full" size="1" title="New" style={{cursor: 'pointer'}}>
-                <PlusIcon width={16} height={16}/>
-            </IconButton>
-        </DialogTrigger>
 
-        <DialogContent style={{maxWidth: '400px'}}>
-            <DialogTitle>Create a Discussion</DialogTitle>
+    return (
+        <DialogRoot>
+            <DialogTrigger>
+                <IconButton
+                    radius="full"
+                    size="1"
+                    title="New"
+                    style={{ cursor: "pointer" }}
+                >
+                    <PlusIcon width={16} height={16} />
+                </IconButton>
+            </DialogTrigger>
 
-            <form action={createDiscussion}>
-                <Flex direction="column" gap="3">
-                    <TextFieldInput placeholder="Name" name="name"/>
-                    <TextArea placeholder="Description" name="description"/>
-                    
-                    <Flex gap="3" mt="4" justify="end">
-                        <DialogClose>
-                            <Button variant="soft" color="gray">Cancel</Button>
-                        </DialogClose>
-                        <DialogClose>
-                            <Button type="submit">Create</Button>
-                        </DialogClose>
+            <DialogContent style={{ maxWidth: "400px" }}>
+                <DialogTitle>Create a Discussion</DialogTitle>
+
+                <form action={createDiscussion}>
+                    <Flex direction="column" gap="2">
+                        <TextFieldInput placeholder="Name" name="name" />
+
+                        <Card>
+                            <Tiptap initialContent="Description">
+                                <TiptapContent />
+                                <TiptapFormCompat />
+                            </Tiptap>
+                        </Card>
+
+                        <Flex gap="3" mt="4" justify="end">
+                            <DialogClose>
+                                <Button variant="soft" color="gray">
+                                    Cancel
+                                </Button>
+                            </DialogClose>
+                            <DialogClose>
+                                <Button type="submit">Create</Button>
+                            </DialogClose>
+                        </Flex>
                     </Flex>
-                </Flex>
-            </form>
-        </DialogContent>
-    </DialogRoot>
+                </form>
+            </DialogContent>
+        </DialogRoot>
+    );
 }
