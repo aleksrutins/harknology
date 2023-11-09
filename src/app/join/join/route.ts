37c7c088c471ca6be1795afb096e0c6141284948
Aleks Rūtiņš <aleks@rutins.com>
2023-11-09 11:30:49 -0500

    Fix join request method

--- src/app/join/join/route.ts
+++ src/app/join/join/route.ts
@@ -2,22 +2,22 @@ import prisma from "@/lib/prisma";
 import { getClassInfo } from "@/app/join/join";
 import { auth } from "@clerk/nextjs";
 
-export async function POST(req: Request) {
-  const { userId } = auth();
-  if (!userId) return new Response("Not authenticated", { status: 403 });
-  const params = new URL(req.url).searchParams;
-  const code = params.get("code") as string | undefined;
-  if (!code) return new Response("Invalid code", { status: 400 });
+export async function GET(req: Request) {
+    const { userId } = auth();
+    if (!userId) return new Response("Not authenticated", { status: 403 });
+    const params = new URL(req.url).searchParams;
+    const code = params.get("code") as string | undefined;
+    if (!code) return new Response("Invalid code", { status: 400 });
 
-  const classData = await getClassInfo(code);
-  if (!classData) return new Response("Invalid code", { status: 400 });
+    const classData = await getClassInfo(code);
+    if (!classData) return new Response("Invalid code", { status: 400 });
 
-  await prisma.studentClass.create({
-    data: {
-      class_id: classData.id,
-      student_id: userId,
-    },
-  });
+    await prisma.studentClass.create({
+        data: {
+            class_id: classData.id,
+            student_id: userId,
+        },
+    });
 
-  return Response.redirect(`/c/${classData.id}`);
+    return Response.redirect(`/c/${classData.id}`);
 }
