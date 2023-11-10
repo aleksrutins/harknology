1245cd47ef29e1729b0fb83b4a162dc55b394caf
Aleks Rutins <aleks@rutins.com>
2023-11-10 01:48:45 +0000

    Basic data structure for discussion scaffolding

--- src/utils/classes.ts
+++ src/utils/classes.ts
@@ -1,30 +1,48 @@
 import prisma from "@/lib/prisma";
 import { cache } from "react";
 
-export const tags = ['classes']
+export const tags = ["classes"];
 
-export const getClasses = cache(async (userId: string) => await prisma.class.findMany({
-    where: {
+export const getClasses = cache(
+  async (userId: string) =>
+    await prisma.class.findMany({
+      where: {
         OR: [
-            { teacher_id: userId },
-            {
-                students: {
-                    some: { student_id: userId }
-                }
-            }
-        ]
-    }
-}))
+          { teacher_id: userId },
+          {
+            students: {
+              some: { student_id: userId },
+            },
+          },
+        ],
+      },
+    })
+);
 
-export const getClass = cache(async (id: string) => await prisma.class.findUnique({
-    where: {
-        id
-    }
-}))
+export const getClass = cache(
+  async (id: string) =>
+    await prisma.class.findUnique({
+      where: {
+        id,
+      },
+    })
+);
 
-export const getMembers = cache(async (id: string) => await prisma.class.findUnique({
-    where: { id },
-    select: { teacher_id: true, students: { select: { student_id: true } } }
-}))
+export const getMembers = cache(
+  async (id: string) =>
+    await prisma.class.findUnique({
+      where: { id },
+      select: { teacher_id: true, students: { select: { student_id: true } } },
+    })
+);
 
-export const canAccessClass = cache(async (classId: string, userId: string) => (!!(await getClasses(userId)).find(c => c.id == classId)))
+export type MembersResponse = ReturnType<typeof getMembers> extends Promise<
+  infer T
+>
+  ? T
+  : never;
+
+export const canAccessClass = cache(
+  async (classId: string, userId: string) =>
+    !!(await getClasses(userId)).find((c) => c.id == classId)
+);
