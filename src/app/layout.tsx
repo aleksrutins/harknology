5065129def4b477b0811ccc6a3c54fe48fed8792
Aleks Rūtiņš <aleks@rutins.com>
2023-11-27 10:07:43 -0500

    Make it a PWA, use Bun

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-import type { Metadata } from 'next'
+import type { Metadata, Viewport } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 import { Theme, ThemePanel } from '@radix-ui/themes'
@@ -8,10 +8,54 @@ import { PageSuspense } from './components/Loader'
 
 const inter = Inter({ subsets: ['latin'] })
 
+const APP_NAME = "Harknology";
+const APP_DEFAULT_TITLE = "Harknology";
+const APP_TITLE_TEMPLATE = "%s - Harknology";
+const APP_DESCRIPTION = "Easily manage your class discussions.";
+
 export const metadata: Metadata = {
-  title: 'Harknology',
-  description: 'Easily manage your class discussions.',
+  title: {
+    default: APP_DEFAULT_TITLE,
+    template: APP_TITLE_TEMPLATE,
+  },
+  description: APP_DESCRIPTION,
+
+  applicationName: APP_NAME,
+  manifest: '/manifest.json',
+
+  metadataBase: process.env.NODE_ENV == 'production' ? new URL('https://harknology-production.up.railway.app/') : undefined,
+
+  appleWebApp: {
+    capable: true,
+    statusBarStyle: "default",
+    title: APP_DEFAULT_TITLE,
+    // startUpImage: [],
+  },
+  formatDetection: {
+    telephone: false,
+  },
+  openGraph: {
+    type: "website",
+    siteName: APP_NAME,
+    title: {
+      default: APP_DEFAULT_TITLE,
+      template: APP_TITLE_TEMPLATE,
+    },
+    description: APP_DESCRIPTION,
+  },
+  twitter: {
+    card: "summary",
+    title: {
+      default: APP_DEFAULT_TITLE,
+      template: APP_TITLE_TEMPLATE,
+    },
+    description: APP_DESCRIPTION,
+  },
 }
+
+export const viewport: Viewport = {
+  themeColor: "color(display-p3 0.319 0.63 0.521)",
+};
 
 export default function RootLayout({
   children,
